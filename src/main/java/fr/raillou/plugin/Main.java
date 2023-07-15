153d3a8a6958435b630051638df1b5d6ab66dbac
BuildTools <unconfigured@null.spigotmc.org>
2023-07-15 14:34:28 +0200

    Probleme plugin.yml

---
+++ src/main/java/fr/raillou/plugin/Main.java
@@ -0,0 +1,10 @@
+package fr.raillou.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        System.out.println("Ca marche !");
+    }
+}
